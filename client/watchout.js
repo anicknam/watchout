20e4cb1a514ac83f7dacb78de7e4e827c076e588
anicknam <a.nicknam@gmail.com>
2016-09-23 16:03:23 -0700

    Added a draggable player

--- client/watchout.js
+++ client/watchout.js
@@ -16,24 +16,26 @@ var randomRange = function(min, max) {
 var generateData = function(n) {
   var results = [];
   for (var i = 0; i < n; i++) {
-    results.push({x: randomRange(radius, window.innerWidth * 0.75 - radius), y: randomRange(radius, window.innerHeight * 0.75 -radius)});
+    results.push({x: randomRange(radius, window.innerWidth * 0.75 - radius), y: randomRange(radius, window.innerHeight * 0.75 - radius)});
   }
   return results;
 };
 
 var sampleData = generateData(30);
 
-var update = function(data){ 
+
+// ENEMIES //////////////////////////////////////////////////////////////////////////////////////////////
+var update = function(data) { 
   var enemies = d3.select('svg').selectAll('.enemies').data(data);
   
   // UPDATE
-
   enemies.transition().duration(1000).attr('cx', function(d) { return d.x; })
     .attr('cy', function(d) { return d.y; });
   
   // ENTER
   enemies.enter().append('circle')
     .attr('class', 'enemies')
+    .attr('fill', 'red')
     .attr('cx', function(d) { return d.x; })
     .attr('cy', function(d) { return d.y; })
     .attr('r', radius);
@@ -45,3 +47,40 @@ setInterval(function() {
   });
   update(newData);
 }, 1500);
+
+// PLAYER //////////////////////////////////////////////////////////////////////////////////////////////
+
+var player = d3.select('svg').selectAll('.players').data([{x: window.innerWidth / 2, y: window.innerHeight / 2}]);
+
+
+// var dragstarted = function(d) {
+//   d3.event.sourceEvent.stopPropagation();
+//   d3.select(this).classed("dragging", true);
+// };
+
+var dragged = function(d) {
+  d3.select(this).attr("cx", d.x = d3.event.x).attr("cy", d.y = d3.event.y);
+};
+
+// var dragended = function(d) {
+//   d3.select(this).classed("dragging", false);
+// };
+
+var drag = d3.behavior.drag()
+  .origin(function(d) { return d; })
+  // .on('dragstart', dragstarted)
+  .on('drag', dragged);
+  // .on('dragend', dragended);
+
+  // player enter
+player.enter().append('circle')
+  .attr('class', 'players')
+  .attr('cx', function(d) { return d.x; })
+  .attr('cy', function(d) { return d.y; })
+  .attr('r', radius)
+  .call(drag);
+
+
+
+
+  
